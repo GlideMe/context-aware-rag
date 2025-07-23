e3d02a33ba5cfed52904dd4d461cab601abf115e
EladEndless <elad@endlessai.com>
2025-07-23 14:26:03 +0300

    NVID:34 | Add log

--- src/vss_ctx_rag/functions/rag/graph_rag/graph_retrieval_func.py
+++ src/vss_ctx_rag/functions/rag/graph_rag/graph_retrieval_func.py
@@ -68,6 +68,8 @@ class GraphRetrievalFunc(Function):
         self.endless_ai_enabled = self.get_param("endless_ai_enabled")
         self.chat_system_prompt = self.get_param("chat_system_prompt", required=False)
 
+        logger.info(f"self.chat_system_prompt: {self.chat_system_prompt}")#TODO: Remove!
+
         try:
             self.graph_retrieval = GraphRetrieval(
                 llm=self.chat_llm,
