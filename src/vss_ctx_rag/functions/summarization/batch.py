eb6a305109b04bab2858bb9c65d6a9a84350bd78
lironhertz <liron@flixwagon.com>
2025-07-27 18:23:33 +0300

    NVID:11 | Get rid of the Dot document so Claud and OpenAI don't get a text saying dot and not images

--- src/vss_ctx_rag/functions/summarization/batch.py
+++ src/vss_ctx_rag/functions/summarization/batch.py
@@ -175,6 +175,10 @@ class BatchSummarization(Function):
                         doc, _, _ = batch.as_list()[0]
                         if doc.strip() == ".":
                             batch_summary = "Video Analysis completed."
+                        else:
+                            batch_summary = await call_token_safe(
+                                {"input": " ".join([doc for doc, _, _ in batch.as_list()]), "images": images}, self.batch_pipeline, self.recursion_limit,
+                            )
             except Exception as e:
                 logger.error(
                     f"Error summarizing batch {batch._batch_index}: {e}"
