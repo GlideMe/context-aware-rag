376766a249a1588953105e8000ffbf52c5249d32
Eran Sadeh-Or <eran@glidetalk.com>
2025-08-19 13:59:39 +0300

    changes following code-review

--- src/vss_ctx_rag/context_manager/context_manager_handler.py
+++ src/vss_ctx_rag/context_manager/context_manager_handler.py
@@ -49,7 +49,6 @@ from vss_ctx_rag.utils.globals import (
     DEFAULT_MULTI_CHANNEL,
     DEFAULT_CHAT_HISTORY,
     DEFAULT_ENDLESS_AI_ENABLED,
-    DEFAULT_ENDLESS_USE_GRIDS_SUMM,
 )
 from vss_ctx_rag.functions.rag.chat_function import ChatFunction
 
@@ -315,10 +314,8 @@ class ContextManagerHandler:
                     summ_config["params"]["batch_size"] = summ_config["params"].get(
                         "batch_size", DEFAULT_BATCH_SUMMARIZATION_BATCH_SIZE
                     )
-                    summ_config["params"]["endless_use_grids_summ"] = summ_config["params"].get(
-                        "endless_use_grids_summ", DEFAULT_ENDLESS_USE_GRIDS_SUMM
-                    )
-                    
+                    summ_config["params"]["endless_use_grids_summ"] = req_info.endless_use_grids_summ
+
                     try:
                         if req_info and req_info.is_live:
                             logger.debug("Req Info: %s", req_info.summary_duration)
