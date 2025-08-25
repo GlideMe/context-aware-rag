c9d6432bb24bd65e669b7b9c7b2c49da8ec8ac3d
eranpolo <eranpolo@gmail.com>
2025-08-25 14:58:39 +0300

    Update graph_extraction.py

--- src/vss_ctx_rag/functions/rag/graph_rag/graph_extraction.py
+++ src/vss_ctx_rag/functions/rag/graph_rag/graph_extraction.py
@@ -466,7 +466,7 @@ class GraphExtraction:
     def fetch_entities_for_embedding(self):
         query = """
                 MATCH (e)
-                WHERE NOT (e:Chunk OR e:Document) AND e.embedding IS NULL AND e.id IS NOT NULL 
+                WHERE NOT (e:Chunk OR e:Document) AND e.embedding IS NULL AND e.id IS NOT NULL
                 RETURN elementId(e) AS elementId, e.id + " " + coalesce(e.description, "") AS text
                 """
         result = self.graph_db.graph_db.query(query)
@@ -474,7 +474,7 @@ class GraphExtraction:
             {"elementId": record["elementId"], "text": record["text"]}
             for record in result
         ]
-    
+
     async def update_embeddings(self, rows):
         with TimeMeasure("GraphExtraction/UpdatEmbding", "yellow"):
             logger.info("update embedding for entities")
